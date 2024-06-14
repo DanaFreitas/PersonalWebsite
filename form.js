ad30a3b2ee648623f89dee5ef53979a7345a4662
Dana Freitas <danarobertfreitas@gmail.com>
2024-06-14 19:12:57 -0400

    Added meta tag

--- form.js
+++ form.js
@@ -2,8 +2,8 @@ const theform = document.getElementById("theform")
 const submit = document.getElementById("submit")
 
 submit.addEventListener("click", (event) => { 
-    event.preventDefault();
-    console.log("Success")
- theform.reset();
+  //  event.preventDefault();
+    //console.log("Success")
+ //theform.reset();
 }
 )
